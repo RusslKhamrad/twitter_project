d4d9546de54856c3d496bee8c5bfc8bd74474f47
ruslan <budkingiv@mail.ru>
2010-07-15 00:08:46 -0700

    Second version

--- twitter/db/seeds.rb
+++ twitter/db/seeds.rb
@@ -1,7 +1,26 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup)
+
+require 'faker'
+require 'populator'
+
+User.destroy_all
+
+10.times do 
+  user = User.new
+  user.username = Faker::Internet.user_name
+  user.email = Faker::Internet.email
+  user.password = "test"
+  user.password_confirmation = "test"
+  user.save
+end
+
+User.all.each do |user|
+  Twit.populate(5..10) do |twit|
+     twit.user_id = user.id
+     twit.message = Faker::Lorem.sentence
+  end
+  3.times do
+     user.add_friend(User.all[rand(User.count)])
+  end
+end
